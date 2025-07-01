29d576bde3c82682a699d07d1b0814f9d1acb68c
pottsie <pottsma@me.com>
2025-06-30 20:01:30 -0400

    Initial Commit

---
+++ PlayerStats/PlayerStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerStatsApp.swift
+//  PlayerStats
+//
+//  Created by Michael Potts on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PlayerStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
