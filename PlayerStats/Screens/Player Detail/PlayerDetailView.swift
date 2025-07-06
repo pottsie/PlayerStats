dcaf4c3c4277738d8c2bc43d62f95a84305f3782
pottsie <pottsma@me.com>
2025-07-06 15:30:27 -0400

    Created an initial layout for the player detail screen

--- PlayerStats/Screens/Player Detail/PlayerDetailView.swift
+++ PlayerStats/Screens/Player Detail/PlayerDetailView.swift
@@ -15,15 +15,75 @@ struct PlayerDetailView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text(player.reverseName)
+                PlayerImage(size: 275)
+                    .padding(.vertical)
+                    .overlay(alignment: .bottom) {
+                        Button {
+                            // MARK: photo picker for choosing a new image
+                        } label: {
+                            Image(systemName: "photo")
+                        }
+                        .foregroundStyle(.blue)
+                        .font(.title)
+                        .offset(x: 120, y: -15)
+                    }
+                
+                VStack(spacing: 15) { // Maybe a form will allow me to break this into seciotns with headers
+                    Text("#10 - Central Midfield")
+                        .font(.title2)
+                        .italic()
+                        .bold()
+                    Text("Captain")
+                        .font(.headline)
+                        .padding(.vertical, 0)
+                    LabeledContent("Birthday", value: "Sep 20, 1966")
+
+                    LabeledContent("Gender", value: "Male")
+                    LabeledContent("Height (cm)", value: "165")
+                    // MARK: the games will be a link to the list of games
+                    LabeledContent("Games") {
+                        Text("9 games")
+                    }
+                    // MARK: the system image icon will take you to the player stats page
+                    LabeledContent("Stats") {
+                        Image(systemName: "chart.bar.fill")
+                    }
+                    LabeledContent("Club", value: player.club)
+                }
+                .font(.title3)
+                .padding(.horizontal)
+                ZStack {
+                    Rectangle()
+                        .fill()
+                        .foregroundStyle(.orange)
+                    Text("Contact Information")
+                        .font(.title3)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+                .frame(width: .infinity, height: 50)
+
+                Spacer()
             }
-            .navigationTitle(player.fullName)
+//            .navigationTitle(player.fullName)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // MARK: switch to editing the player information
+                    } label: {
+                        Text("Edit")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
     }
 }
 
 #Preview {
     let preview = Preview(Player.self)
-    PlayerDetailView(player: Player.samplePlayers[1])
-        .modelContainer(preview.container)
+    NavigationStack {
+        PlayerDetailView(player: Player.samplePlayers[1])
+            .modelContainer(preview.container)
+    }
 }
